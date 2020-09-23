8b6d56fc37b591ca4300cca5d42f735f40799971
Miqueias <miqueias.caetano@doctum.edu.br>
2020-09-23 16:52:44 -0300

    ok

--- index.php
+++ index.php
@@ -1062,26 +1062,6 @@ if (!$ambiente_desenvolvimento) {
             </div>
           </div>
         </section>
-
-        <section>
-          <div class="form-row">
-            <div class="form-group col-md-12">
-              <h2 class="mt-4">Política de Privacidade</h2>
-
-              <p>Este site pode utilizar cookies e/ou web beacons quando um usuário tem acesso às páginas. Os cookies que podem ser utilizados associam-se (se for o caso) unicamente com o navegador de um determinado computador.</p>
-
-              <p>Os cookies que são utilizados neste site podem ser instalados pelo mesmo, os quais são originados dos distintos servidores operados por este, ou a partir dos servidores de terceiros que prestam serviços e instalam cookies e/ou web beacons (por exemplo, os cookies que são empregados para prover serviços de publicidade ou certos conteúdos através dos quais o usuário visualiza a publicidade ou conteúdos em tempo pré-determinados).</p>
-
-              <p>Usamos empresas de publicidade de terceiros para veicular anúncios durante a sua visita ao nosso website. Essas empresas podem usar informações (que não incluem o seu nome, endereço, endereço de e-mail ou número de telefone) sobre suas visitas a este e a outros websites a fim de exibir anúncios relacionados a produtos e serviços de seu interesse.</p>
-
-              <p>O Google, como fornecedor de terceiros, utiliza cookies para exibir anúncios neste site.</p>
-              <p>Com o cookie DART, o Google pode exibir anúncios para o usuário com base nas visitas feitas à este ou à outros sites na Internet.</p>
-              <p>O usuário pode desativar o cookie DART <a href="https://policies.google.com/technologies/ads" target="_BLANK">visitando a Política de privacidade da rede de conteúdo e dos anúncios do Google</a>.</p>
-
-              <p>O usuário têm a possibilidade de configurar seu navegador para ser avisado, na tela do computador, sobre a recepção dos cookies e para impedir a sua instalação no disco rígido. As informações pertinentes a esta configuração estão disponíveis em instruções e manuais do próprio navegador.</p>
-            </div>
-          </div>
-        </section>
       </div> 
     </section>
     <!-- /#page-content-wrapper -->
--- sidebar.php
+++ sidebar.php
@@ -6,16 +6,16 @@
   </div>
   <div class="list-group list-group-flush">
     <a href="./" class="list-group-item list-group-item-action bg-light">Gerador de Pessoa</a>
-    <a href="./gerar_nomes" class="list-group-item list-group-item-action bg-light">Gerador de Nomes</a>
-    <a href="./gerar_cnh" class="list-group-item list-group-item-action bg-light">Gerador de CNH</a>
-    <a href="./gerar_rg" class="list-group-item list-group-item-action bg-light">Gerador de RG</a>
-    <a href="./gerar_tituloeleitor" class="list-group-item list-group-item-action bg-light">Gerador de Título de Eleitor</a>
-    <a href="./gerar_cpf" class="list-group-item list-group-item-action bg-light">Gerador de CPF</a>
-    <a href="./gerar_pispasep" class="list-group-item list-group-item-action bg-light">Gerador de PIS/PASEP</a>
-    <a href="./gerar_cnpj" class="list-group-item list-group-item-action bg-light">Gerador de CNPJ</a>
-    <a href="./gerar_certidaonascimento" class="list-group-item list-group-item-action bg-light">Gerador de Certidão de Nascimento</a>
-    <a href="./gerar_certidaocasamento" class="list-group-item list-group-item-action bg-light">Gerador de Certidão de Casamento</a>
-    <a href="./gerar_certidaoobito" class="list-group-item list-group-item-action bg-light">Gerador de Certidão de Óbito</a>
+    <a href="./gerar_nomes.php" class="list-group-item list-group-item-action bg-light">Gerador de Nomes</a>
+    <a href="./gerar_cnh.php" class="list-group-item list-group-item-action bg-light">Gerador de CNH</a>
+    <a href="./gerar_rg.php" class="list-group-item list-group-item-action bg-light">Gerador de RG</a>
+    <a href="./gerar_tituloeleitor.php" class="list-group-item list-group-item-action bg-light">Gerador de Título de Eleitor</a>
+    <a href="./gerar_cpf.php" class="list-group-item list-group-item-action bg-light">Gerador de CPF</a>
+    <a href="./gerar_pispasep.php" class="list-group-item list-group-item-action bg-light">Gerador de PIS/PASEP</a>
+    <a href="./gerar_cnpj.php" class="list-group-item list-group-item-action bg-light">Gerador de CNPJ</a>
+    <a href="./gerar_certidaonascimento.php" class="list-group-item list-group-item-action bg-light">Gerador de Certidão de Nascimento</a>
+    <a href="./gerar_certidaocasamento.php" class="list-group-item list-group-item-action bg-light">Gerador de Certidão de Casamento</a>
+    <a href="./gerar_certidaoobito.php" class="list-group-item list-group-item-action bg-light">Gerador de Certidão de Óbito</a>
    
 <!--     <a href="./gerar_carteiratrabalho" class="list-group-item list-group-item-action bg-light">Gerador de Carteira de Trabalho</a>
     <a href="./gerar_endereco" class="list-group-item list-group-item-action bg-light">Gerador de Comprovante de Endereço</a>
